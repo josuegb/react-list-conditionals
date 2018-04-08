fa768b2e938a8797da7e7c090ace364b0252ec6f
josuegb15 <josueg@thehangar.cr>
2018-04-08 11:06:13 -0600

    Validation Component

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 const validationComponet = (props) => {
     let textValidationResult = 'Text too short';
+    let textValidationResultColor = 'red';
+
+    if (props.textLegnth > 4) {
+      textValidationResult = 'Text long enough';
+      textValidationResultColor = 'green';
+    }
+
+    const style = {
+      color: textValidationResultColor
+    };
 
     return (
-        <div>
-
-        </div>
+      <div>
+        <p style={style}>{textValidationResult}</p>
+      </div>
     );
 }
 
