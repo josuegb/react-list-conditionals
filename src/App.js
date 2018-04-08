797bbf7e4fced7c02490e3268c6fcf93d35fcb4d
josuegb15 <josueg@thehangar.cr>
2018-04-08 10:30:06 -0600

    two binding input

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: '',
+    textLegnth: 0
+  }
+
+  inputOnChangeHandler = (event) => {
+    const text = event.target.value;
+    const textLegnth = text.length;
+
+    this.setState({
+      text,
+      textLegnth
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React: lists and condiotionals</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input className="App-main-input" onChange={this.inputOnChangeHandler} value={this.state.text}/>
+        <p>Text length: {this.state.textLegnth}</p>
+        <p>Text: {this.state.text}</p>        
       </div>
     );
   }
